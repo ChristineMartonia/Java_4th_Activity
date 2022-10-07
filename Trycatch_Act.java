88066a2dbac9d3b73729307dbd6b66f0da6af2d0
Sheesh! <106144313+tinytinnn@users.noreply.github.com>
2022-10-07 15:23:55 +0800

    Update Trycatch_Act.java

--- Trycatch_Act.java
+++ Trycatch_Act.java
@@ -27,31 +27,31 @@ public class TrycatchActivity {
         p1.setPetName("Lilly"); 
         p1.setPetColor("Brown");
         p1.setPetType("Dog");
-        Pet.add("\"" + p1.getPetName() + "\"" + " She is a" + p1.getPetColor() + " colored "+ p1.getPetType());
+        Pet.add("\"" + p1.getPetName() + "\"" + " She is a " + p1.getPetColor() + " colored "+ p1.getPetType());
     
         Pet<String> p2 = new Pet<String>();
         p2.setPetName("Oreo"); 
         p2.setPetColor("Black and white");
         p2.setPetType("Bird");
-        Pet.add("\"" + p2.getPetName() + "\"" + " He is a" + p2.getPetColor() + " colored "+ p2.getPetType());
+        Pet.add("\"" + p2.getPetName() + "\"" + " He is a " + p2.getPetColor() + " colored "+ p2.getPetType());
 
         Pet<String> p3 = new Pet<String>();
         p3.setPetName("Thunder"); 
         p3.setPetColor("Gray");
         p3.setPetType("Dog");
-        Pet.add("\"" + p3.getPetName() + "\"" + " He is a" + p3.getPetColor() + " colored "+ p3.getPetType());
+        Pet.add("\"" + p3.getPetName() + "\"" + " He is a " + p3.getPetColor() + " colored "+ p3.getPetType());
    
         Pet<String> p4 = new Pet<String>();
         p4.setPetName("Rin"); 
         p4.setPetColor("white");
         p4.setPetType("Cat");
-        Pet.add("\"" + p4.getPetName() + "\"" + " She is a" + p4.getPetColor() + " colored "+ p4.getPetType());
+        Pet.add("\"" + p4.getPetName() + "\"" + " She is a " + p4.getPetColor() + " colored "+ p4.getPetType());
 
         Pet<String> p5 = new Pet<String>();
         p5.setPetName("Mooky"); 
         p5.setPetColor("white");
         p5.setPetType("Hamster");
-        Pet.add("\"" + p5.getPetName() + "\"" + " She is a" + p5.getPetColor() + " colored "+ p5.getPetType()); 
+        Pet.add("\"" + p5.getPetName() + "\"" + " She is a " + p5.getPetColor() + " colored "+ p5.getPetType()); 
 
 
         try {
